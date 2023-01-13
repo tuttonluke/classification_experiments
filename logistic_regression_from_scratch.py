4a1dccc91df6e147650ae0b30cc975fe183873b5
Luke Tutton <tuttonluke@gmail.com>
2023-01-13 13:49:50 +0000

    Adds fit method.

--- logistic_regression_from_scratch.py
+++ logistic_regression_from_scratch.py
@@ -1,51 +1,62 @@
 # %%
+import numpy as np
+from sklearn.linear_model import LogisticRegression
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import MinMaxScaler
 # %%
-class MyLogisticRegression:
-    def __init__(self, n_features) -> None:
-        self.W = np.random.randn(n_features)
-        self.b = np.random.randn()
+def min_max_norm(X, y):
+    sX = MinMaxScaler()
+    sy = MinMaxScaler()
 
-    def fit(self, X_train, y_train):
-        # get W and b from linear regression
-        self.linear_regression(X_train, y_train)        
+    scaled_X = sX.fit_transform(X)
+    scaled_y = sy.fit_transform(y.reshape(-1, 1))
 
-    def predict(self, X):
-        # apply sigmoid function to get probability
-        prediction = self.sigmoid(X)
-        return prediction
+    return scaled_X, scaled_y
+# %%
+class LogitsticRegression:
+    def __init__(self, learning_rate, n_epochs) -> None:
+        self.learning_rate = learning_rate
+        self.epochs = n_epochs
 
-    def sigmoid(self, num):
-        p = 1 / (1 + np.power(np.e, -num))
-        return p
-    
-    def binary_cross_entropy(self):
+    def fit(self, X, y):
+        """Function for training the model.
+
+        Parameters
+        ----------
+        X : np.array
+            Matrix of feature data.
+        y : np.array
+            Vector of label data.
+        """
+        self.no_training_examples, self.no_features = X.shape
+        # initialise weights and bias
+        self.W = np.zeros(self.no_features)
+        self.b = 0
+        self.X = X
+        self.y = y
+
+        # gradient descent optimisation
+        for i in range(self.epochs):
+            self.update_weights()
+        
+        return self
+
+    def update_weights(self):
         pass
-    
-    def linear_regression(self, X_train, y_train):
-        model = LinearRegression()
-        model.fit(X_train, y_train)
-        self.W = model.coef_
-        self.b = model.intercept_
-
-# %%
+# %% 
 if __name__ == "__main__":
-    np.random.seed(42)
     X, y = datasets.load_breast_cancer(return_X_y=True)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
     X_test, X_validation, y_test, y_validation = train_test_split(X_test, 
-                                                                    y_test, test_size=0.3)
+                                                                    y_test, test_size=0.3)    
+    X_train_scaled, y_train_scaled = min_max_norm(X_train, y_train)
 
-    model = MyLogisticRegression(n_features=30)
-    model.fit(X_train, y_train)
-    my_y_pred_val = model.predict(X_validation)
 
-# %% TEST WITH SKLEARN
-np.random.seed(42)
-log_model = LogisticRegression(max_iter=10000)
-log_model.fit(X_train, y_train)
-y_pred_val = model.predict(X_validation)
+    # TEST WITH SKLEARN
+    np.random.seed(42)
+    log_model = LogisticRegression(max_iter=10000)
+    log_model.fit(X_train, y_train)
+    y_pred_val = log_model.predict(X_validation)
+    
+# %%
