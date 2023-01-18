f38d0b811e24112092a85354b8f3c87322b85092
Luke Tutton <tuttonluke@gmail.com>
2023-01-18 12:51:04 +0000

    Adds random seed.

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import sklearn.datasets
 import numpy as np
 import matplotlib.pyplot as plt
 # %%
+np.random.seed(42)
 colors = [
  '#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', 
  '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', 
